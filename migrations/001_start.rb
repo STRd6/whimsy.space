f3e4f7f271cb1a3787bd8098ca1b477d191dd312
Daniel X Moore <yahivin@gmail.com>
2015-04-06 19:30:24 -0700

    Added some domain constraints

--- migrations/001_start.rb
+++ migrations/001_start.rb
@@ -2,8 +2,13 @@ Sequel.migration do
   change do
     create_table :people do
       Integer :id, primary: true
+
       String :email, null: false, unique: true
+
       String :domain, null: false, unique: true
+      constraint(:domain_min_length) {char_length(domain) >= 3}
+      constraint(:domain_max_length) {char_length(domain) <= 16}
+
       String :persistent_token, null: false, unique: true
     end
   end
